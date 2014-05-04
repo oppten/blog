bed8ac362575636578419a5a0f297f63ba0ba633
nickmancol <nicolas@oppten.com>
2014-05-04 08:54:35 -0400

    Themed with bootstrap3

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Oppten'
+SITENAME = u'Oppten Blog'
+SITEURL = 'http://oppten.github.io'
+THEME = u'pelican-bootstrap3'
+BOOTSTRAP_THEME = u'cosmo'
+TIMEZONE = 'America/Bogota'
+
+DEFAULT_LANG = u'es'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Oppten', 'http://oppten.com/'),
+          ('VidaNP', 'http://vidanp.wordpress.com/'),
+          ('Nasa', 'http://nasa.gov/'),
+          ('Ypursite?', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
